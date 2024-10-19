96f6dd73b1f92ff0a5875ebe1de3973bd0fb1e12
unknown <wesleytsakane116@gmail.com>
2024-10-19 23:57:28 +0200

    connect server

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const connectDB = require('./db');
+require('dotenv').config();
+connectDB();
 app.use(express.json());
 
 
